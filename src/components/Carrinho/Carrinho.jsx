522f014cd6a30d790085a166b5f9709cf0d2aca0
Lucas Ferreira <lsousaah@gmail.com>
2023-08-10 16:41:42 -0300

    add: conteúdo base do componente Carrinho

--- src/components/Carrinho/Carrinho.jsx
+++ src/components/Carrinho/Carrinho.jsx
@@ -1,7 +1,29 @@
+import './Carrinho.css'
+
+function MenuCarrinho() {
+  return (
+    <div className='menu_cart'>
+    <div className='menu_total'>//Aqui fica o total//</div>
+
+    <div className='menu_confirm'>
+    <button className='menu_confirm_metodo' >METODO</button> 
+    <button className='menu_confirm_finalizar'>FINALIZAR</button>
+    </div>
+    </div>
+  )
+}
+
+
+
+
+
 function Carrinho() {
+
+
   return (
     <div>
-     
+
+      <MenuCarrinho/>
     </div>
   );
 }
