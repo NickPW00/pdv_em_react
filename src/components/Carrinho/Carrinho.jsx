81155aa8b08d7ae04c85d77caa9ffdc7c99fb4a5
Lucas Ferreira <lsousaah@gmail.com>
2023-08-10 17:26:52 -0300

    add: avanços na estilizaçao e comentários

--- src/components/Carrinho/Carrinho.jsx
+++ src/components/Carrinho/Carrinho.jsx
@@ -4,8 +4,8 @@ import './Carrinho.css'
 
 
 function MenuCarrinho() {
+  //pro botao do metodo de pagmaneto
   const [mostrarMetodo, setMostrarMetodo] = useState(false);
-
   const handleBotaoClick = () => {
     setMostrarMetodo(true);
   };
@@ -14,12 +14,18 @@ function MenuCarrinho() {
 
   return (
     <div className='menu_cart'>
-    <div className='menu_total'>//Aqui fica o total//</div>
-    <div className='menu_confirm'>
-      
+
+    <div className='menu_total'>
+      <div className='menu_total_rs'>TOTAL: R$</div>
+      <div className='menu_total_resultado'>RESULTADO</div>
+    </div>
+
+    <div className='menu_confirm'>  
+    {/* AQUI É ONDE VC CLICA EM METODO E ABRE O COMPONENTE METODO QUE SERÁ CRIADO */}
     <button className='menu_confirm_metodo' id='btn_cart' onClick={handleBotaoClick} >METODO</button> 
     {mostrarMetodo && <Metodo/>}
 
+    {/* NESSE CASO SERÁ EM BREVECRIADO UM COMPONENTE PARA FINALIZAR A COMPRA, PROVAVELMENTE UM POPUP */}
     <button className='menu_confirm_finalizar' id='btn_cart' >FINALIZAR</button>
     </div>
     </div>
@@ -31,7 +37,8 @@ function Carrinho() {
 
   return (
     <div className='container_cart'>
-
+        {/* AQUI VAI FICAR O CONTAINER COM OS ITENS QUE FORAM SELECIONADOS NO "CAIXA" */}
+        {/* E O MENU CARRINHO VAI FICAR NO FIM DO CONTAINER_CART */}
       <MenuCarrinho/>
     </div>
   );
