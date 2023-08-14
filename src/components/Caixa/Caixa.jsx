214646a294d953f86741d11078cb71126818d712
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-08-14 00:50:30 -0300

    Aplicação de nome do produto ser visivel após pôr corretamente o codigo feito; identação de parte do codigo feita.

--- src/components/Caixa/Caixa.jsx
+++ src/components/Caixa/Caixa.jsx
@@ -1,5 +1,6 @@
 import './Caixa.css';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import produtosData from '../../data/data.json'
 
 /* Componente base para os botões. */
 function Teclas({ numero, funcao, className }) {
@@ -8,11 +9,30 @@ function Teclas({ numero, funcao, className }) {
   )
 }
 
+function InputsDiversos({onClick , className, value}) {
+  return (
+    <div onClick={onClick}>
+      <input className={className} type='text' value={value} disabled/>
+    </div>
+  )
+}
+
 export default function EscreverCodigo() {
-  const [codigo, setCodigo] = useState('')
-  const [quant, setQuant] = useState('')
-  const [selecionado, setSelecionado] = useState(true)
-  const numeros = []
+  const [codigo, setCodigo] = useState('');
+  const [quant, setQuant] = useState('');
+  const [produto, setProduto] = useState('');
+  const [selecionado, setSelecionado] = useState(true);
+  const numeros = [];
+
+  useEffect(() => {
+    const produtoEncontrado = produtosData.find(produto => produto.codigo === codigo);
+    console.log("Estou aqui")
+    if (produtoEncontrado) {
+      setProduto(produtoEncontrado.nome); // Defina qual propriedade do produto deseja exibir
+    } else {
+      setProduto('Produto não encontrado');
+    }
+  }, [codigo])
 
   /* Um FOR para criar os numeros que quer nas Teclas, para mais tarde, fazer um map */
   for(let i = 9; i >= 0; i--){
@@ -34,17 +54,21 @@ export default function EscreverCodigo() {
 
   return (
     <div className='caixa'>
-      <input type='text' disabled/>
+      <input type='text' value={produto} disabled/>
       {/* Inputs desabilitados para não serem acessados pelo teclado do computador. */}
       {/* Inputs envoltos em div para que assim as divs contenham a função que faça a conversão de onde será escrito. */}
-      <div onClick={() => setSelecionado(false)}>
-        <input className={`caixa__codigo_input ${selecionado ? '' : 'borda_grossa'}`} type='text' value={codigo}  disabled/>
-      </div>
+      <InputsDiversos 
+        className={`caixa__codigo_input ${selecionado ? '' : 'borda_grossa'}`}
+        onClick={() => setSelecionado(false)}
+        value={codigo}
+      />
       <div className='caixa__quant'>
         <span>Quantidade</span>
-        <div onClick={() => setSelecionado(true)}>
-          <input className={`caixa__quant_input ${selecionado ? 'borda_grossa' : ''}`} type='text' value={quant} disabled/>
-        </div>
+        <InputsDiversos 
+          onClick={() => setSelecionado(true)}
+          className={`caixa__quant_input ${selecionado ? 'borda_grossa' : ''}`}
+          value={quant}
+        />
       </div>
       <div className='teclado'>
       {/* Map para criar as teclas com numero. */}
