b399d4cd8f02e89f0e4e79b881f587aff2c693ad
Nicholas Puggian <nicholas.wright@al.infnet.edu.br>
2023-08-31 15:33:23 -0300

    Arquivos da biblioteca Router adicionados e pequenas mudanças no index feitas em relação a nova ferramenta.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Caixa from './components/Caixa/Caixa'
+import OpenCart from './components/OpenCart/OpenCart';
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Caixa />, 
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
